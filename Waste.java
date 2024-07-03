5500205777368e4d7bd2eb97e951bff2a9bf0569
Buwa <buwa@Buwas-MacBook-Pro.local>
2024-07-04 01:13:20 +0200

    waste management rest api initialised

--- Waste.java
+++ Waste.java
@@ -1,5 +1,36 @@
 package com.enviro.assessment.grad001.buwamabasa.enviro_waste_management;
 
 public class Waste {
-
+	private long id;
+	private String state;
+	private String type;
+	
+	//Constructor
+	//Getters
+	//toString
+	
+	public Waste(long id, String state, String type) {
+		super();
+		this.id = id;
+		this.state = state;
+		this.type = type;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public String getName() {
+		return state;
+	}
+	public String getAuthor() {
+		return type;
+	}
+	
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + state + ", author=" + type + "]";
+	}
+	
+	
 }
+
--- WasteController.java
+++ WasteController.java
@@ -3,14 +3,17 @@ package com.enviro.assessment.grad001.buwamabasa.enviro_waste_management;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class WasteController {
-	// /wasteManagement
-	// Waste: id, name, type
+	
+	@RequestMapping("/wastes")
 	public List<Waste> retrieveAllWastes() {
 		return Arrays.asList(
 				new Waste(1, "Solid", "General"),
-				new Waste(2, "Solid", "Hazardous"),
-		)
+				new Waste(2, "Solid", "Hazardous")
+		);
 	}
 }
