d5dd25831c1cb2dca52650d1b2f0be016d4330c1
Buwa <buwa@Buwas-MacBook-Pro.local>
2024-07-04 11:10:43 +0200

    Changed the model to use the categories of wastes and the waste item to determine the recycling bin the item belongs to

--- Waste.java
+++ Waste.java
@@ -2,33 +2,38 @@ package com.enviro.assessment.grad001.buwamabasa.enviro_waste_management;
 
 public class Waste {
 	private long id;
-	private String state;
-	private String type;
+	private String category;
+	private String item;
+	private String bin;
 	
 	//Constructor
 	//Getters
 	//toString
 	
-	public Waste(long id, String state, String type) {
+	public Waste(long id, String category, String item, String bin) {
 		super();
 		this.id = id;
-		this.state = state;
-		this.type = type;
+		this.category = category;
+		this.item = item;
+		this.bin = bin;
 	}
 	
 	public long getId() {
 		return id;
 	}
-	public String getName() {
-		return state;
+	public String getCategory() {
+		return category;
 	}
-	public String getAuthor() {
-		return type;
+	public String getItem() {
+		return item;
+	}
+	public String getBin() {
+		return bin;
 	}
 	
 	@Override
 	public String toString() {
-		return "Course [id=" + id + ", name=" + state + ", author=" + type + "]";
+		return "Waste [id=" + id + ", category=" + category + ", item=" + item + ", bin=" + bin + "]";
 	}
 	
 	
--- WasteController.java
+++ WasteController.java
@@ -6,14 +6,16 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//http://localhost:8080/wastes
 @RestController
 public class WasteController {
 	
 	@RequestMapping("/wastes")
 	public List<Waste> retrieveAllWastes() {
 		return Arrays.asList(
-				new Waste(1, "Solid", "General"),
-				new Waste(2, "Solid", "Hazardous")
+				new Waste(1, "Plastic", "plastic container", "Recycling bin"),
+				new Waste(2, "Glass", "Glass bottle", "Recycling bin"),
+				new Waste(3, "Organic", "Vegetable peels", "Compost bin")
 		);
 	}
 }
