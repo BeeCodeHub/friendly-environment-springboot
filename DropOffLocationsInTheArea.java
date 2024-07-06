baa86f978cfe5bd1102b9801da930c30e3a8a96c
Buwa <buwa@Buwas-MacBook-Pro.local>
2024-07-06 17:41:14 +0200

    drop off location added for the hazardous waste

---
+++ DropOffLocationsInTheArea.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.enviro.assessment.grad001.buwamabasa.enviro_waste_management;
+
+/**
+ * 
+ */
+public class DropOffLocationsInTheArea {
+
+	/**
+	 * 
+	 */
+	public DropOffLocationsInTheArea() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
