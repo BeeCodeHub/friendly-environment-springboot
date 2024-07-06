454e1e48d1c1030b6d917195d318fcb53a106147
Buwa <buwa@Buwas-MacBook-Pro.local>
2024-07-06 17:45:01 +0200

    pickup request for heavy waste added

---
+++ PickupRequest.java
@@ -0,0 +1,14 @@
+package com.enviro.assessment.grad001.buwamabasa.enviro_waste_management;
+
+public class PickupRequest {
+
+	public PickupRequest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
